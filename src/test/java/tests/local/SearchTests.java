3b4d0cd802e18fe7c00cdfe97ed2f15daf7f1ce4
svasenkov <aanher@gmail.com>
2022-06-17 21:40:32 +0300

    added test for android real

--- src/test/java/tests/local/SearchTests.java
+++ src/test/java/tests/local/SearchTests.java
@@ -2,18 +2,33 @@ package tests.local;
 
 import com.codeborne.selenide.CollectionCondition;
 import io.appium.java_client.AppiumBy;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
+@Tag("android")
 public class SearchTests extends TestBase {
     @Test
     void searchTest() {
         back();
         step("Type search", () -> {
             $(AppiumBy.id("org.wikipedia.alpha:id/search_container")).click();
-            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("BrowserStack");
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
+        });
+        step("Verify content found", () ->
+                $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
+                        .shouldHave(CollectionCondition.sizeGreaterThan(0)));
+    }
+
+    @Test
+    void searchWithByTextLocatorTest() {
+        back();
+        switchTo().alert().accept();
+        step("Type search", () -> {
+            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
         });
         step("Verify content found", () ->
                 $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
